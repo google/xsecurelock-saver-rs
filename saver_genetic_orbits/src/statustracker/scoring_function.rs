f10b44caefb412faf383a27721852fe213cf86b0
Zachary Stewart <zstewart@google.com>
2018-11-29 07:55:47 -0500

    add header to scoring_function

--- saver_genetic_orbits/src/statustracker/scoring_function.rs
+++ saver_genetic_orbits/src/statustracker/scoring_function.rs
@@ -1,3 +1,17 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /// Expression for computing the per-frame score for a scene from that frame's total mass and total
 /// mass count and the tick count.
 // TODO(zstewar1): Find a better way to handle parsing. S-expressions as a yaml string?
